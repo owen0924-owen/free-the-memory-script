06a4266382401c6ceb3ebf005f7cd64022e6385c
owen0924 <64713824+owen0924-owen@users.noreply.github.com>
2023-01-24 17:14:01 +0800

    Create free.sh

---
+++ free.sh
@@ -0,0 +1,7 @@
+sudo sh -c 'echo 1 >/proc/sys/vm/drop_caches'
+
+sudo sh -c 'echo 2 >/proc/sys/vm/drop_caches'
+
+sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'
+
+output "Done! Check Your Memory By Executing Command htop Or btop."
