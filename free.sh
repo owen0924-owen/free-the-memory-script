96499b2bf51fcc1b418dbaeee9ced849eb23956d
owen0924 <64713824+owen0924-owen@users.noreply.github.com>
2023-01-24 17:16:58 +0800

    Update free.sh

--- free.sh
+++ free.sh
@@ -4,4 +4,4 @@ sudo sh -c 'echo 2 >/proc/sys/vm/drop_caches'
 
 sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'
 
-output "Done! Check Your Memory By Executing Command htop Or btop."
+echo "Done! Check Your Memory By Executing Command htop Or btop."
